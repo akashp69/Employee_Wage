e99ea48f362e0a1cc7c82a7bb0e6f37ecd12d400
akashp69 <akashpakhare499@gmail.com>
2022-11-29 23:10:51 +0530

    Added: Program For Calculate Daily Employee Wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,26 +1,30 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
+
     /**
-     * This Method is Used For Check The Attendance Of Employees
+     * This Method is Used For Check The Attendance Of Employees and Calculate Daily Wage
      */
-    static void checkEmployeePresentOrAbsent(){
-    /**
-     *Using present and employeeCheck Variable for getting attendance of Employee
-     */
-        int present = 1;
-        int employeeCheck = (int) Math.floor(Math.random()*10)%2;
-        if (employeeCheck == present) {
-            System.out.println("Employee is Present");
-        }
-        else
-            System.out.println("Employee is Absent");
-    }
+     static void checkEmployeePresentOrAbsent() {
+         int present = 1;
+         int absent = 0;
+         int empWagePerHour = 20;
+         int fullDayHour = 8;
+         int empcheck = (int) Math.floor(Math.random() * 10) % 2;
+         if (empcheck == present) {
+             System.out.println("Employee is Present");
+             int dailyWage = empWagePerHour * fullDayHour;
+             System.out.println("Daily Wage of Employee is" + dailyWage);
+         } else {
+             System.out.println("Employee is Absent");
+             System.out.println("Daily Wage of Employee is Zero");
+         }
+         }
     /**
      * This Main Method is Used For Calling The Method checkEmployeePresentOrAbsent
      */
+    public static void main(String[] args) {
+         checkEmployeePresentOrAbsent();
 
-    public static void main(String args[]) {
-        checkEmployeePresentOrAbsent();
     }
-}
+    }
