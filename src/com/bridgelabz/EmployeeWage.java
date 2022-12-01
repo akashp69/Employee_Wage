c2c73e27371641d44c8502d6ebd458f05338d3b3
akashp69 <akashpakhare499@gmail.com>
2022-12-02 00:58:30 +0530

    Added: Program For Calculate The Employee Wages For Multiple Companies

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -2,7 +2,7 @@ package com.bridgelabz;
 
 public class EmployeeWage {
     /**
-     * Using Static And final keyword for fixed hours for part-time and full-time
+     * Using Final keyword and static variables for fixed hours
      */
     static final int Total_Working_Hours = 100;
     static final int WAGE_PER_HOUR = 20;
@@ -13,10 +13,14 @@ public class EmployeeWage {
     static final int WORKING_DAYS_PER_MONTH = 20;
 
     /**
-     * This Static Method is Using For Calculate the Monthly Total working hours
+     * This is static Method Used For calculate Employees wages of Differents Companny
+     * @param companyName using variables for company
+     * @param empperhour using variables for calculate the per hour working
+     * @param totalworkingdays using variables for calculate the total  hour working in day
+     * @param totaldayinmonths using variables for calculate the total  hour working  days in month
      */
 
-    static void employeeWage(){
+    static void EmployeeWage(String companyName , int empperhour , int totalworkingdays , int totaldayinmonths){
         int day = 1;
         int totalWorkingHours = 0;
         int totalWage = 0;
@@ -48,15 +52,19 @@ public class EmployeeWage {
     }
 
     /**
-     *This is Main Method is Used for calling the employeeWage Static method
-     *
+     *This is Main Method Is Using For Calling Static Method
      */
 
     public static void main(String[] args) {
-
-        System.out.println("Welcome to Employee Wage");
-        employeeWage();
-
+        System.out.println("Welcome to Employee Wage For Multiples Company ");
+        System.out.println("\n SBI BANK ");
+        EmployeeWage("SBI" ,100, 20 ,25);
+        System.out.println("\n**********************");
+        System.out.println("\n AXIS BANK ");
+        EmployeeWage("AXIS BANK " ,250, 25 ,28);
+        System.out.println("\n**********************");
+        System.out.println("\n ICICI BANK ");
+        EmployeeWage("ICICI BANK" ,500, 15 ,30);
     }
 
 }
