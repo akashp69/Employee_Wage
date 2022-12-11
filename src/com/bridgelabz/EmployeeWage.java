d1e4e15ceff6c421e01468bfb7d3fbc689f5429c
akashp69 <akashpakhare499@gmail.com>
2022-12-11 16:03:53 +0530

    Added: Program for save the Total Wages for each Companies

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,70 +1,91 @@
 package com.bridgelabz;
-
 public class EmployeeWage {
     /**
-     * Using Final keyword and static variables for fixed hours
+     * Using the static and final keyword for fixed time
      */
-    static final int Total_Working_Hours = 100;
-    static final int WAGE_PER_HOUR = 20;
-    static final int FULL_TIME_HOUR = 8;
-    static final int PART_TIME_HOUR = 4;
-    static final int IS_FULL_TIME = 2;
-    static final int IS_PART_TIME = 1;
-    static final int WORKING_DAYS_PER_MONTH = 20;
-
+    static final int PART_TIME = 1;
+    static final int FULL_TIME = 2;
     /**
-     * This is static Method Used For calculate Employees wages of Differents Companny
-     * @param companyName using variables for company
-     * @param empperhour using variables for calculate the per hour working
-     * @param totalworkingdays using variables for calculate the total  hour working in day
-     * @param totaldayinmonths using variables for calculate the total  hour working  days in month
+     * Using instances variables for diffrent companies
+     */
+    final String COMPANY_NAME;
+    final int WAGE_PER_HR;
+    final int MAX_WORKING_DAYS;
+    final int MAX_WORKING_HRS;
+    /**
+     * Using instances variables for calculate the total wages of diffrent companies
      */
 
-    static void EmployeeWage(String companyName , int empperhour , int totalworkingdays , int totaldayinmonths){
-        int day = 1;
-        int totalWorkingHours = 0;
-        int totalWage = 0;
-        while (day <= WORKING_DAYS_PER_MONTH && totalWorkingHours<=Total_Working_Hours) {
-            int dailyWage = 0;
-            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+    int totalWage;
 
-            switch (empCheck) {
-                case IS_FULL_TIME:
-                    System.out.println("Employee FULL TIME");
-                    dailyWage = WAGE_PER_HOUR * FULL_TIME_HOUR;
-                    totalWorkingHours=totalWorkingHours + FULL_TIME_HOUR;
-                    break;
-                case IS_PART_TIME:
-                    System.out.println("Employee PART TIME");
-                    dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
-                    totalWorkingHours=totalWorkingHours + PART_TIME_HOUR;
-                    break;
-                default:
-                    System.out.println("Employee Absent");
+    EmployeeWage(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs) {
+        COMPANY_NAME = companyName;
+        WAGE_PER_HR = wagePerHr;
+        MAX_WORKING_DAYS = maxWorkingDays;
+        MAX_WORKING_HRS = maxWorkingHrs;
+        totalWage = 0;
+    }
+    /**
+     * Using non-static method for check employee parttime , fulltime and absent
+     */
+    int generateEmployeeType() {
+        return (int) (Math.random() * 100) % 3;
+    }
+    /**
+     * Using non-static method for calculate the working hours of  different companies employes
+     */
+    int getWorkingHrs(int empType) {
+        switch (empType) {
+            case FULL_TIME:
+                return 8;
 
-            }
-            totalWage = totalWage + dailyWage;
-            System.out.println("Daily Wage => " + dailyWage);
-            day++;
+            case PART_TIME:
+                return 4;
+            default:
+                return 0;
         }
-        System.out.println("Total Wage" +totalWage);
-        System.out.println("Total Working Hours "+totalWorkingHours);
+    }
+    /**
+     * Using non-static method for calculate total wage for different companies employees
+     */
+    void calculateTotalWage() {
+        System.out.println("Computation of total wage of " + COMPANY_NAME + "employee");
+        System.out.println("-------------------------------------------------------------------");
+        System.out.printf("%5s     %5s     %5s     %5s\n", "Day", "Workinghrs", "Wage", "Total working hrs");
+        int workingHrs = 0;
+        for (int day = 1, totalWorkingHrs = 0; day <= MAX_WORKING_DAYS
+                && totalWorkingHrs <= MAX_WORKING_HRS; day++, totalWorkingHrs += workingHrs) {
+
+            int empType = generateEmployeeType();
+            workingHrs = getWorkingHrs(empType);
+            int wage = workingHrs * WAGE_PER_HR;
+            totalWage += wage;
+            System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHrs, wage, totalWorkingHrs + workingHrs);
+        }
     }
 
+    public String toString() {
+        System.out.println("Details of " + COMPANY_NAME + " employee");
+        System.out.println("-----------------------------------------------------");
+        System.out.println("Wage per hour: " + WAGE_PER_HR);
+        System.out.println("Maximum working days:" + MAX_WORKING_DAYS);
+        System.out.println("Maximum working hours:" + MAX_WORKING_HRS);
+        return "Total wage for a month of " + COMPANY_NAME + " employee is " + totalWage + "\n";
+    }
     /**
-     *This is Main Method Is Using For Calling Static Method
+     * This is a Main Method is using for calling the methods
      */
+    public static void main(String args[]) {
+        EmployeeWage sbi = new EmployeeWage("SBI", 8, 20, 100);
+        EmployeeWage axis = new EmployeeWage("AXIS", 5, 30, 150);
+        EmployeeWage icici = new EmployeeWage("ICICI", 8, 18, 110);
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage For Multiples Company ");
-        System.out.println("\n SBI BANK ");
-        EmployeeWage("SBI" ,100, 20 ,25);
-        System.out.println("\n**********************");
-        System.out.println("\n AXIS BANK ");
-        EmployeeWage("AXIS BANK " ,250, 25 ,28);
-        System.out.println("\n**********************");
-        System.out.println("\n ICICI BANK ");
-        EmployeeWage("ICICI BANK" ,500, 15 ,30);
+        sbi.calculateTotalWage();
+        System.out.println(sbi);
+        axis.calculateTotalWage();
+        System.out.println(axis);
+        icici.calculateTotalWage();
+        System.out.println(icici);
     }
 
 }
